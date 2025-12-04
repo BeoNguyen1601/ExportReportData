21823ff2df71756a8628162ce8cf172d81c9928f
BeoNguyen1601 <>
2025-12-04 21:53:52 +0700

    update code

--- ExportData/Form1.cs
+++ ExportData/Form1.cs
@@ -93,7 +93,7 @@ namespace ExportData
             {
                 InitializeComponent();
                 var currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
-                this.Text = $"Export Data v{currentVersion} mới tinh";
+                this.Text = $"Export Data v{currentVersion}";
             }
             catch (Exception)
             {
